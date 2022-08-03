6d6e31e0cb143cb35856af7993f087efda4aa54d
nathalia <nathaliatroina@gmail.com>
2022-08-03 13:13:03 -0400

    routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,28 @@
-
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import key from '../../key';
+//even though we write everything on the scss file we still import the css file
+
+
 
 function App() {
   return (
-  <div></div>
+  <div> 
+    {/* anything we put up here will display in all the pages no matter what. The same thing apply below the routes as well */}
+    <div>festinha</div>
+    <Routes>
+      <Route path='/info' element={
+        <div>informations</div>
+      } />
+      <Route path='/' element={
+        <div>test</div>
+      } />
+      <Route path='/*' element={
+        // the * will display the especifico elemento for all none especifico path
+        <div>Home</div>
+      } />
+    </Routes>
+    </div>
 
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
-
+import {BrowserRouter as Router} from 'react-router-dom';
+//I rename BrowserRouter as Router in this way I can use the name Router instead BrowserRouter
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>
+  </Router>
 );
 
