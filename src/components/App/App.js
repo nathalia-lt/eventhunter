e6c55dce4e7f6181e8ee5677629ce342c5e2b4e3
nathalia <nathaliatroina@gmail.com>
2022-08-03 13:43:24 -0400

    axios

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,25 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes} from 'react-router-dom';
 import './App.css';
 import key from '../../key';
 //even though we write everything on the scss file we still import the css file
-
+import  React, {useEffect, useState} from 'react';
+import axios from 'axios'
 
 
 function App() {
+
+let [eventData, setEventData] = useState([])
+
+  let url = 'https://app.ticketmaster.com/discovery/v2/events.json?apikey=' + key
+
+
+  useEffect(() => {
+    axios.get(url)
+    .then(r => setEventData(r.data))
+  },[]) 
+
+
+
   return (
   <div> 
     {/* anything we put up here will display in all the pages no matter what. The same thing apply below the routes as well */}
