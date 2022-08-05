73763676bd46d3f42203edd18556098c95de28eb
nathalia <nathaliatroina@gmail.com>
2022-08-05 09:38:22 -0400

    events to display

--- src/components/EventContainer/EventContainer.js
+++ src/components/EventContainer/EventContainer.js
@@ -3,15 +3,20 @@ import EventCard from "../EventCard/EventCard"
 
 export default function EventContainer({ eventData }) { 
     let allEvents = eventData._embedded.events
-    console.log(allEvents)
+    let eventsToDisplay = allEvents.map(event => {
+        return (
+            <EventCard
+            key={event.id}
+            event={event}
+            />
+        )
+    })
     let event = eventData._embedded.events[0]
     //console.log(event)
     
     return (
         <div className='eventContainer'>
-            <EventCard
-            event={event}
-            />
+            {eventsToDisplay}
         </div>
     )
 }
