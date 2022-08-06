ff383ff64c03803250df32645b18538a4a5d3c60
nathalia <nathaliatroina@gmail.com>
2022-08-06 17:41:25 -0400

    mapped filter tabs

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,34 +1,49 @@
-import React, {useState} from "react"
+import React, { useState } from "react"
 
-export default function Filter(){
+export default function Filter() {
 
- //update my 'filter' everytime that I click on the category, I need to put a value in my div and them do the event target value
+    //update my 'filter' everytime that I click on the category, I need to put a value in my div and them do the event target value
 
- let [genreFilter, setGenreFilter] = useState('all')
-
- function handleGenreFilterClick(event) {
-     setGenreFilter(event.target.textContent.toLowerCase())
- }
+    let [genreFilter, setGenreFilter] = useState('all')
 
 
-    return(
-        <div className='eventContainerCategories'>
-                <div className="eventCategory">
-                    <img src="https://img.icons8.com/small/500/000000/music.png" />
-                    <div onClick={handleGenreFilterClick} >Music</div>
-                </div>
-                <div className="eventCategory">
-                    <img src="https://img.icons8.com/ios/500/000000/football2--v1.png" />
-                    <div onClick={handleGenreFilterClick} >Sports</div>
-                </div>
-                <div className="eventCategory">
-                    <img src="https://img.icons8.com/ios/500/000000/movie-projector.png" />
-                    <div onClick={handleGenreFilterClick} >Theater</div>
-                </div>
-                <div className="eventCategory">
-                    <img src="https://img.icons8.com/ios/500/000000/kiss-panda--v1.png" />
-                    <div onClick={handleGenreFilterClick} >Kids</div>
-                </div>
+
+    //
+
+    let allGenres = ['All', 'Sports', 'Music', 'Theater', 'kids']
+
+    let filterCategoriesToDisplay = allGenres.map(genre => {
+        function handleGenreFilterClick() {
+            setGenreFilter(genre.toLowerCase())
+        }
+        return (
+            <div className="eventCategory">
+                <img onClick={handleGenreFilterClick} src="https://img.icons8.com/small/500/000000/music.png" />
+                <div onClick={handleGenreFilterClick} >{genre}</div>
             </div>
+        )
+    })
+
+    return (
+        <div className='eventContainerCategories'>
+            {filterCategoriesToDisplay}
+
+            {/* <div className="eventCategory">
+                <img src="https://img.icons8.com/small/500/000000/music.png" />
+                <div onClick={handleGenreFilterClick} >Music</div>
+            </div>
+            <div className="eventCategory">
+                <img src="https://img.icons8.com/ios/500/000000/football2--v1.png" />
+                <div onClick={handleGenreFilterClick} >Sports</div>
+            </div>
+            <div className="eventCategory">
+                <img src="https://img.icons8.com/ios/500/000000/movie-projector.png" />
+                <div onClick={handleGenreFilterClick} >Theater</div>
+            </div>
+            <div className="eventCategory">
+                <img src="https://img.icons8.com/ios/500/000000/kiss-panda--v1.png" />
+                <div onClick={handleGenreFilterClick} >Kids</div>
+            </div> */}
+        </div>
     )
 }
