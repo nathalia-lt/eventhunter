cecf581a032e2c7cec9ce97c3e482c75874d8c96
nathalia <nathaliatroina@gmail.com>
2022-08-06 17:06:56 -0400

    add functions to event category

--- src/components/EventContainer/EventContainer.js
+++ src/components/EventContainer/EventContainer.js
@@ -20,12 +20,11 @@ export default function EventContainer({ eventData }) {
 
     //update my 'filter' everytime that I click on the category, I need to put a value in my div and them do the event target value
 
-    let [update, setUpdate] = useState('all')
+    let [genreFilter, setGenreFilter] = useState('all')
 
-    function handleUpdateCategory(event) {
-        setUpdate(event.target.textContent.toLowerCase())
+    function handleGenreFilterClick(event) {
+        setGenreFilter(event.target.textContent.toLowerCase())
     }
-
 
 
     return (
@@ -35,25 +34,20 @@ export default function EventContainer({ eventData }) {
             <div className='eventContainerCategories'>
                 <div className="eventCategory">
                     <img src="https://img.icons8.com/small/500/000000/music.png" />
-                    <div>Music</div>
+                    <div onClick={handleGenreFilterClick} >Music</div>
                 </div>
                 <div className="eventCategory">
                     <img src="https://img.icons8.com/ios/500/000000/football2--v1.png" />
-                    <div>Sports</div>
+                    <div onClick={handleGenreFilterClick} >Sports</div>
                 </div>
                 <div className="eventCategory">
                     <img src="https://img.icons8.com/ios/500/000000/movie-projector.png" />
-                    <div>Theater</div>
+                    <div onClick={handleGenreFilterClick} >Theater</div>
                 </div>
                 <div className="eventCategory">
                     <img src="https://img.icons8.com/ios/500/000000/kiss-panda--v1.png" />
-                    <div>Kids</div>
+                    <div onClick={handleGenreFilterClick} >Kids</div>
                 </div>
-                {/* <div className='eventCategory' value='all' onClick={handleUpdateCategory} >All</div>
-        <div className='eventCategory' value='music' onClick={handleUpdateCategory} >Music</div>
-        <div className='eventCategory'value='sports' onClick={handleUpdateCategory} >Sports</div>
-        <div className='eventCategory' value='theater' onClick={handleUpdateCategory} >Theater</div>
-        <div className='eventCategory'value='kids' >Kids</div> */}
             </div>
             <div className='eventContainer'>
                 {eventsToDisplay}
