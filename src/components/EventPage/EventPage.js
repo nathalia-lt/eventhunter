c8a730548388c3b9464cc3555e8c2479ff762da5
nathalia <nathaliatroina@gmail.com>
2022-08-09 10:50:36 -0400

    setting tab name of the event

--- src/components/EventPage/EventPage.js
+++ src/components/EventPage/EventPage.js
@@ -5,16 +5,19 @@ import React, { useEffect, useState } from "react"
 
 
 export default function EventPage(){
+    let [eventPageData, setEventPageData] =useState({}) //eventData always going to be an object
+    document.title=`${eventPageData.name}` //here is to show on the tab the name of the event
 
     let params = useParams() //its going to look at the params in our path.
     console.log(params)
+
 
     //Now, I am going to do a fetch, it means that everytime that I click on event on my page it is going to
     //fetch to an especific event and show in my 'new eventpage'
 
     let id = params.id
 
-    let [eventPageData, setEventPageData] =useState('')
+
 
 
     let url = 'https://app.ticketmaster.com' +'/discovery/v2/events/' + id + '?locale=en-us' + '&apikey=' + key
